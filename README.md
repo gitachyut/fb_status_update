b8e758ea3810d1cdc3bdbd177f9e01d5f1c09667
gitachyut <linkinakd72@gmail.com>
2016-10-19 15:46:28 +0530

    env is setup

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# fb_status_update
-Facebook Status Update App Using Facebook Open Graph
+## Facebook Status Update App Using Facebook Open Graph
--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 const express = require('express');
+const fs = require('fs');
+const exec = require('child_process').exec;
 const app = express();
-var exec = require('child_process').exec;
 const PORT = 9000;
+const env = require('node-env-file');
+env(__dirname + '/.env');
+
 app.use(express.static(__dirname+'/public'));
-app.use('/',(req,res)=>{
-  res.render('index.ejs');
+
+app.get('/',(req,res)=>{
+  var access_token = process.env.access_token;
+  if(access_token){
+    access_token = true;
+  }else{
+    access_token = false;
+  }
+  res.render('index.ejs',{access_token});
+});
+app.post('/fb_status_update',(req,res)=>{
+  console.log(req);
 })
 app.listen(PORT,()=>{
   console.log('app statred');
-  exec(`open http://localhost:${PORT}/`);
+  // exec(`open http://localhost:${PORT}/`);
 })
